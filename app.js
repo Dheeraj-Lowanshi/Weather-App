d5cecf1858837877392c9520f7f328b7509c8004
Dheeraj Lowanshi <164254408+Dheeraj-Lowanshi@users.noreply.github.com>
2024-09-17 18:49:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ let urlend = "&appid=97a9745b0c3a1f932357060a2331ab49&units=metric";
 let result = document.getElementById("result");
 let btn = document.getElementById("search-btn");
 btn.addEventListener("click", () => {
-  let city = `Delhi`;
-  city = document.getElementById("inp-word").value;
+  let city = document.getElementById("inp-word").value;
   if (city.length == 0) {
     alert(`Please type in a city name`);
   }
-  window.addEventListener("load", () => {
-    fetch(`${urlstart}${city}${urlend}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const description = data.weather[0].description;
-        console.log(data);
-        result.innerHTML = `      
+
+  fetch(`${urlstart}${city}${urlend}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const description = data.weather[0].description;
+      console.log(data);
+      result.innerHTML = `      
         <div id="city-name"><i class="fa-solid fa-city icon"></i> City : ${data.name}</div>
         <div id="temperature"><i class="fa-solid fa-temperature-three-quarters icon"></i> Temperature : ${data.main.temp} °C</div>
         <div id="description"><i class="fa-solid fa-cloud icon"></i> Discription : ${data.weather[0].description}</div>
@@ -23,13 +22,13 @@ btn.addEventListener("click", () => {
         <div id="wind"><i class="fa-solid fa-wind icon"></i> Wind Speed : ${data.wind.speed} Km/h</div>
         <div id="visibility"><i class="fa-solid fa-eye icon"></i> Visibility : ${data.visibility} Meter </div>
         `;
-      })
-      .catch((error) => {
-        result.innerHTML = `
+    })
+    .catch((error) => {
+      result.innerHTML = `
       <div id="word">
           <h3>${`Could't find weather`}</h3>
       </div>`;
-      });
-  });
+    });
 });
+
 /* Search City Data End */
